ca8832da38a1b9937ce0c9442176cbd3bfa481d1
unknown <vyankat2013@gmail.com>
2022-08-16 20:51:54 +0530

    created new repository and adding files

---
+++ src/javav/myintro.java
@@ -0,0 +1,57 @@
+package javav;
+
+public class myintro {
+
+	public static void main(String[] args) 
+	{
+		// my name
+		// my name
+		
+		/* value 
+		 * 
+		 * add
+		 * remove 
+		 * edit
+		 * 
+		 * 
+		 * 
+		 * */
+		String name;
+		name="Vyankat";
+		System.out.println(name);
+		
+		
+		// TODO Auto-generated method stub
+		
+	
+		System.out.println("Hi Gud Evng");
+	
+		int rollnum;
+		
+		rollnum=10;
+		System.out.println(rollnum);
+		System.out.println("My roll number is"+rollnum);
+		
+		System.out.println("My roll number is "+rollnum);
+		
+		
+		float weight;
+		
+		weight=10.7f;
+		
+		System.out.println(weight);
+		
+		System.out.println("My weight is "+weight);
+		
+		System.out.println("My weight is "+weight+"Kg");
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
